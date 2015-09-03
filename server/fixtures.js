23c4d94315b26a07fb9e29bd54b059cad2d6a1c9
zwicker <emmet@mallpup.com>
2015-09-03 21:45:58 +1000

    Publish files

---
+++ server/fixtures.js
@@ -0,0 +1,21 @@
+if (Posts.find().count() === 0) {
+	
+	Posts.insert({
+		title: 'Introducing Telescope',
+		url: 'http://sachagreif.com/introducing-telescope',
+		flagged: false
+	});
+	
+	Posts.insert({
+		title: 'Meteor',
+		url: 'http://meteor.com',
+		flagged: true
+	});
+	
+	Posts.insert({
+		title: 'The Meteor Book',
+		url: 'http://themeteorbook.com',
+		flagged: false
+	});
+
+}
